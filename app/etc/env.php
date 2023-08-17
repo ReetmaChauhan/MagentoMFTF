6e5369e0cdc63ac506b26774ec8ca3cfc435ae9a
ReetmaChauhan <reetma@infino.co>
2023-08-17 10:46:56 +0530

    errsgfsgf

---
+++ app/etc/env.php
@@ -0,0 +1,85 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'LhH53EP3WPe3mKgN24Pct86G8YIGp38p'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '863_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '863_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '27422ffd85e5d4c0561f950f990d6bca'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'mysql',
+                'dbname' => 'farmboy',
+                'username' => 'farmboy',
+                'password' => 'farmboy',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'localhost'
+    ],
+    'install' => [
+        'date' => 'Mon, 07 Aug 2023 09:11:26 +0000'
+    ]
+];
